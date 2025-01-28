21387cd6cca14db725a211012a963ec32ae14699
ahmed12v <ahnurmed16@gmail.com>
2025-01-28 19:42:00 +0200

    Athou Done

--- src/app/core/Athountcation/new-password/new-password.component.ts
+++ src/app/core/Athountcation/new-password/new-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-password.component.html',
   styleUrl: './new-password.component.css'
 })
--- src/app/core/Athountcation/verify-code/verify-code.component.ts
+++ src/app/core/Athountcation/verify-code/verify-code.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-verify-code',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './verify-code.component.html',
   styleUrl: './verify-code.component.css'
 })
