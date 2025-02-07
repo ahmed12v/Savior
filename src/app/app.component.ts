cf126cd0c36476829464cb9eb17f11a38ab4d67d
ahmed12v <ahnurmed16@gmail.com>
2025-02-07 17:26:03 +0200

    doctor background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './core/importantCompnent/footer/footer.componen
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent , FooterComponent],
+  imports: [RouterOutlet , NavbarComponent , FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/particles-component/particles-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particles-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './particles-component.component.html',
+  styleUrl: './particles-component.component.css'
+})
+export class ParticlesComponentComponent {
+
+}
