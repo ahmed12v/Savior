c65b0add8028f67a559832a2a63921469d60734f
ahmed12v <ahnurmed16@gmail.com>
2025-03-16 22:02:19 +0200

    doc okay

--- src/app/component/doctor/book-form/book-form.component.ts
+++ src/app/component/doctor/book-form/book-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-form',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.css'
 })
