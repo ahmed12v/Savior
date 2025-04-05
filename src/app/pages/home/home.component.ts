f90df8e562ac4ba96056016258124dd323e689ed
ahmed12v <ahnurmed16@gmail.com>
2025-04-05 21:51:09 +0200

    ckeck

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent  {
-
+  details:any[]= [
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+      route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
+      },
+  ]
  
 }
