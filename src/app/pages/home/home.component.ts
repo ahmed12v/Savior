0ef7f7fc1594d269a72635338e742763da315faa
ahmed12v <ahnurmed16@gmail.com>
2025-04-05 22:03:12 +0200

    uupdate

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,22 +12,22 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent  {
   details:any[]= [
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
-     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, add it to your cart, and then it will be delivered to you through us. ',
+     {tittle:'Pharmacy & Medicine' , detals:'You can search for the nearest pharmacy, search for the treatment you want, and then it will be delivered to you through us. ',
       route:'/pharmcy' , icon:' fa-solid fa-prescription-bottle-medical fs-2 icon-des '
       },
   ]
