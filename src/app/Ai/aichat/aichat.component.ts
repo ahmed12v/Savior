8971dcd0b097fb64849a4c76634d49e7bf045398
ahmed12v <ahnurmed16@gmail.com>
2025-05-04 23:43:39 +0300

    depoloy

--- src/app/Ai/aichat/aichat.component.ts
+++ src/app/Ai/aichat/aichat.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AichatService } from '../../shared/services/Ai/aichat.service';
-import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-aichat',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [],
   templateUrl: './aichat.component.html',
   styleUrls: ['./aichat.component.css']
 })
